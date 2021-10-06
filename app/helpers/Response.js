2d2f871b806cb755ca6bd000cf489bb32b893609
Salah Awad <salah.awad@outlook.com>
2021-10-06 21:40:21 +0200

    Update Response.js

--- app/helpers/Response.js
+++ app/helpers/Response.js
@@ -6,40 +6,35 @@ module.exports = {
         response.data = data;
         response.status = 200;
         response.success = true;
-        res.status=200;
-        return res.json(response);
+        return res.status(200).json(response);
     },
     notOk: function (res, message = undefined) {
         let response = {};
         response.message = message;
         response.status = 200;
         response.success = false;
-        res.status=200;
-        return res.json(response);
+        return res.status(200).json(response);
     },
     exist: function (res, message = undefined) {
         let response = {};
         response.message = message;
         response.status = 409;
         response.success = false;
-        res.status=409;
-        return res.json(response);
+        return res.status(409).json(response);
     },
     wronginfo: function (res, message = undefined) {
         let response = {};
         response.message = message;
         response.status = 401;
         response.success = false;
-        res.status=401;
-        return res.json(response);
+        return res.status(401).json(response);
     },
     notfound: function (res, message = undefined) {
         let response = {};
         response.message = message;
         response.status = 404;
         response.success = false;
-        res.status=404;
-        return res.json(response);
+        return res.status(401).json(response);
     },
 
 };
